8f14a20aa2eacff6ea9e5bccd3b427c4e0e89198
Seth Reeser <seth.reeser@devopsgroup.io>
2017-05-26 12:51:58 -0400

    Refinement through quantity.

--- README.md
+++ README.md
@@ -8,16 +8,18 @@ Moai /ˈmoʊ.aɪ/ provides competitive intelligence by tracking the unique regul
 
 The below data is free, looking for a complete picture with valuable insights? Please contact us at info@devopsgroup.io to learn more.
 
-| Drug | Indication | Generic | Update frequency |
-| ---- | ---------- | ------ | ---------------- |
-| [4ventavis.com](http://4ventavis.com)<br>Actelion | pulmonary arterial hypertension | iloprost systemic | ![4ventavis.com](data/4ventavis.com.png) |
-| [adcirca.com](http://adcirca.com)<br>United Therapeutics | pulmonary arterial hypertension | tadalafil systemic | ![adcirca.com](data/adcirca.com.png) |
-| [letairis.com](http://letairis.com)<br>Gilead | pulmonary arterial hypertension | ambrisentan systemic | ![letairis.com](data/letairis.com.png) |
-| [opsumit.com](http://opsumit.com)<br>Actelion | pulmonary arterial hypertension | macitentan systemic | ![opsumit.com](data/opsumit.com.png) |
-| [orenitram.com](http://orenitram.com)<br>United Therapeutics | pulmonary arterial hypertension | treprostinil systemic | ![orenitram.com](data/orenitram.com.png) |
-| [remodulin.com](http://remodulin.com)<br>United Therapeutics | pulmonary arterial hypertension | treprostinil systemic | ![remodulin.com](data/remodulin.com.png) |
-| [revatio.com](http://revatio.com)<br>Pfizer | pulmonary arterial hypertension | sildenafil systemic | ![revatio.com](data/revatio.com.png) |
-| [tracleer.com](http://tracleer.com)<br>Actelion | pulmonary arterial hypertension | bosentan systemic | ![tracleer.com](data/tracleer.com.png) |
-| [tyvaso.com](http://tyvaso.com)<br>United Therapeutics | pulmonary arterial hypertension | treprostinil systemic | ![tyvaso.com](data/tyvaso.com.png) |
-| [uptravi.com](http://uptravi.com)<br>Actelion | pulmonary arterial hypertension | selexipag systemic | ![uptravi.com](data/uptravi.com.png) |
-| [veletri.com](http://veletri.com)<br>Actelion | pulmonary arterial hypertension | epoprostenol systemic | ![veletri.com](data/veletri.com.png) |
+### Pulmonary Arterial Hypertension
+
+| Drug | Company | Generic | Update frequency |
+| ---- | ------- | ------- | ---------------- |
+| [4ventavis.com](http://4ventavis.com) | Actelion | iloprost systemic | ![4ventavis.com](data/4ventavis.com.png) |
+| [adcirca.com](http://adcirca.com) | United Therapeutics | tadalafil systemic | ![adcirca.com](data/adcirca.com.png) |
+| [letairis.com](http://letairis.com) | Gilead | ambrisentan systemic | ![letairis.com](data/letairis.com.png) |
+| [opsumit.com](http://opsumit.com) | Actelion | macitentan systemic | ![opsumit.com](data/opsumit.com.png) |
+| [orenitram.com](http://orenitram.com) | United Therapeutics | treprostinil systemic | ![orenitram.com](data/orenitram.com.png) |
+| [remodulin.com](http://remodulin.com) | United Therapeutics | treprostinil systemic | ![remodulin.com](data/remodulin.com.png) |
+| [revatio.com](http://revatio.com) | Pfizer | sildenafil systemic | ![revatio.com](data/revatio.com.png) |
+| [tracleer.com](http://tracleer.com) | Actelion | bosentan systemic | ![tracleer.com](data/tracleer.com.png) |
+| [tyvaso.com](http://tyvaso.com) | United Therapeutics | treprostinil systemic | ![tyvaso.com](data/tyvaso.com.png) |
+| [uptravi.com](http://uptravi.com) | Actelion | selexipag systemic | ![uptravi.com](data/uptravi.com.png) |
+| [veletri.com](http://veletri.com) | Actelion | epoprostenol systemic | ![veletri.com](data/veletri.com.png) |
--- moai.py
+++ moai.py
@@ -17,6 +17,7 @@ def dict_constructor(loader, node):
 yaml.add_representer(collections.OrderedDict, dict_representer)
 yaml.add_constructor(_mapping_tag, dict_constructor)
 
+
 # get our data
 with open('data.yml', 'r') as stream:
     try:
@@ -25,34 +26,37 @@ with open('data.yml', 'r') as stream:
         print 'There was a problem loading the yml file...'
         print(exception)
 
+
 # find regulatory code changes
-for website in data['websites']:
-    # what website?
-    print website
+for indication in data:
 
-    # get the html
-    html_content = urllib2.urlopen('http://' + website).read()
-    # search for the code using the regex defined per website
-    live_matches = re.findall(data['websites'][website]['regex'], html_content);
+    for website in data[indication]:
+        # what website?
+        print website
 
-    # get the most recent date
-    most_recent_date = data['websites'][website]['dates'].keys()[-1]
-    # get the most recent data code
-    most_recent_date_code = data['websites'][website]['dates'][most_recent_date]['code']
+        # get the html
+        html_content = urllib2.urlopen('http://' + website).read()
+        # search for the code using the regex defined per website
+        live_matches = re.findall(data[indication][website]['regex'], html_content);
 
-    # if we find a match
-    if len(live_matches) > 0:
-        if str(most_recent_date_code) == str(live_matches[0]):
-            print '\t- The most recent code that we have ' + str(most_recent_date) + ':' + str(most_recent_date_code) + ' matches what we just found ' + str(live_matches[0])
+        # get the most recent date
+        most_recent_date = data[indication][website]['dates'].keys()[-1]
+        # get the most recent data code
+        most_recent_date_code = data[indication][website]['dates'][most_recent_date]['code']
+
+        # if we find a match
+        if len(live_matches) > 0:
+            if str(most_recent_date_code) == str(live_matches[0]):
+                print '\t- The most recent code that we have ' + str(most_recent_date) + ':' + str(most_recent_date_code) + ' matches what we just found ' + str(live_matches[0])
+            else:
+                print '\t- Found a different code than the one we have, writing ' + str(live_matches[0]) + ' to data.yml'
+                data[indication][website]['dates'][int(time.strftime("%Y%m%d"))] = { 'code' : str(live_matches[0]) }
+                with open('data.yml', 'w') as outfile:
+                    yaml.dump(data, outfile, default_flow_style=False)
+
+        # if we dont find a match
         else:
-            print '\t- Found a different code than the one we have, writing ' + str(live_matches[0]) + ' to data.yml'
-            data['websites'][website]['dates'][int(time.strftime("%Y%m%d"))] = { 'code' : str(live_matches[0]) }
-            with open('data.yml', 'w') as outfile:
-                yaml.dump(data, outfile, default_flow_style=False)
-
-    # if we dont find a match
-    else:
-        print '\t- Did not find any live matches, please check that the regex is current.'
+            print '\t- Did not find any live matches, please check that the regex is current.'
 
 
 # generate images
@@ -64,36 +68,40 @@ import pandas as pd
 # set global styles
 matplotlib.rcParams.update({'font.size': 6})
 
-content = "| Drug | Indication | Generic | Update frequency |"
-content += "\n| ---- | ---------- | ------ | ---------------- |"
+for indication in data:
 
-for website in data['websites']:
+    content = '\n### ' + str(indication)
+    content += '\n'
+    content += '\n| Drug | Company | Generic | Update frequency |'
+    content += '\n| ---- | ------- | ------- | ---------------- |'
 
-    dates = []
+    for website in data[indication]:
 
-    for date in data['websites'][website]['dates']:
+        dates = []
 
-        dates.append(str(date))
+        for date in data[indication][website]['dates']:
 
-    X = pd.to_datetime(dates)
-    print X
-    fig, ax = plt.subplots(figsize=(6,0.4))
-    ax.scatter(X, [1]*len(X), marker='v', s=50, color='#306caa')
-    fig.autofmt_xdate()
+            dates.append(str(date))
 
-    # turn off unncessary items
-    ax.yaxis.set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
+        X = pd.to_datetime(dates)
+        print X
+        fig, ax = plt.subplots(figsize=(6,0.4))
+        ax.scatter(X, [1]*len(X), marker='v', s=50, color='#306caa')
+        fig.autofmt_xdate()
 
-    ax.get_yaxis().set_ticklabels([])
-    day = pd.to_timedelta("1", unit='D')
-    plt.xlim(X[0] - day, X[-1] + day)
-    plt.savefig('data/' + website + '.png', bbox_inches='tight')
+        # turn off unncessary items
+        ax.yaxis.set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.xaxis.set_ticks_position('bottom')
 
-    content += '\n| [{0}](http://{0})<br>{1} | {2} | {3} | ![{0}](data/{0}.png) |'.format(website, data['websites'][website]['drug']['company'], data['websites'][website]['drug']['indication'], data['websites'][website]['drug']['generic'])
+        ax.get_yaxis().set_ticklabels([])
+        day = pd.to_timedelta("1", unit='D')
+        plt.xlim(X[0] - day, X[-1] + day)
+        plt.savefig('data/' + website + '.png', bbox_inches='tight')
+
+        content += '\n| [{0}](http://{0}) | {1} | {2} | ![{0}](data/{0}.png) |'.format(website, data[indication][website]['drug']['company'], data[indication][website]['drug']['generic'])
 
 
 # generate  README.md
@@ -107,7 +115,6 @@ f.write('''
 Moai /ˈmoʊ.aɪ/ provides competitive intelligence by tracking the unique regulatory code on United States pharmaceutical websites that are mandated by the FDA. This provides insight as to when, and how often, a website is updated.
 
 The below data is free, looking for a complete picture with valuable insights? Please contact us at info@devopsgroup.io to learn more.
-
 {0}
 '''.format(content))
 f.close()
