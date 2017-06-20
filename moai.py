d57c7dcf49288918caf02fc60072de1c1ce2adf0
Seth Reeser <seth.reeser@devopsgroup.io>
2017-06-20 15:20:56 -0400

    Data, data, everywhere!

--- moai.py
+++ moai.py
@@ -11,6 +11,7 @@ DEVELOPMENT NOTES
 
 import base64
 import collections
+import datetime
 import gzip
 import hashlib
 import hmac
@@ -504,8 +505,6 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
-# set global styles
-matplotlib.rcParams.update({'font.size': 6})
 
 content = '<table>'
 
@@ -518,14 +517,14 @@ for indication in data:
     content += '<td colspan="8"><strong>' + str(indication) + '</strong></td>'
     content += '</tr>'
     content += '\n<tr>'
-    content += '<td>Drug \ generic \ company</td>'
-    content += '<td>HTTPS \ server \ ASN</td>'
-    content += '<td>:iphone:</td>'
-    content += '<td>:wheelchair:</td>'
-    content += '<td>:computer:</td>'
-    content += '<td>:link:</td>'
-    content += '<td>:trophy:</td>'
-    content += '<td>Regulatory code update frequency</td>'
+    content += '<td>Drug<br/>generic<br/>company</td>'
+    content += '<td><img src="https://placehold.it/15/cc4c02?text=+">Regulatory code</td>'
+    content += '<td><img src="https://placehold.it/15/fe9929?text=+">HTTPS<br/><img src="https://placehold.it/15/fee391?text=+">server<br/><img src="https://placehold.it/15/ffffe5?text=+">ASN</td>'
+    content += '<td>:iphone:<br/><img src="https://placehold.it/15/014636?text=+"></td>'
+    content += '<td>:wheelchair:<br/><img src="https://placehold.it/15/016c59?text=+"></td>'
+    content += '<td>:computer:<br/><img src="https://placehold.it/15/02818a?text=+"></td>'
+    content += '<td>:link:<br/><img src="https://placehold.it/15/C0C0C0?text=+"></td>'
+    content += '<td>:trophy:<br/><img src="https://placehold.it/15/FFDF00?text=+"></td>'
     content += '</tr>'
 
     for website in data[indication]:
@@ -533,41 +532,94 @@ for indication in data:
         # what website?
         print('\n' + website + '\n------------------------------').upper()
 
-        # generate date plots for code changes
-        dates = []
+        # set the figure size
+        ax = plt.figure(figsize=(12,1.5), dpi=60)
+        # set the font size
+        matplotlib.rcParams.update({'font.size': 8})
+        # qualitative color palette picker
+        # http://colorbrewer2.org/#type=qualitative&scheme=Paired&n=10
+
+        # plot code
         for date in data[indication][website]['dates']:
             if 'code' in data[indication][website]['dates'][date]:
-                dates.append(str(date))
-        X = pd.to_datetime(dates)
-        print(X)
-        fig, ax = plt.subplots(figsize=(6,0.4))
-        ax.scatter(X, [1]*len(X), marker='v', s=50, color='#306caa')
-        fig.autofmt_xdate()
+                plt.axvline(x=datetime.datetime.strptime(str(date), '%Y%m%d'), color='#cc4c02')
 
-        # generate date plots for https changes
-        dates = []
+        # plot https
         for date in data[indication][website]['dates']:
             if 'https' in data[indication][website]['dates'][date]:
-                dates.append(str(date))
-        X = pd.to_datetime(dates)
-        print(X)
-        fig, ax = plt.subplots(figsize=(6,0.4))
-        ax.scatter(X, [1]*len(X), marker='v', s=50, color='#FF0000')
-        fig.autofmt_xdate()
+                plt.axvline(x=datetime.datetime.strptime(str(date), '%Y%m%d'), color='#fe9929')
 
-        # turn off unncessary items
-        ax.yaxis.set_visible(False)
-        ax.spines['right'].set_visible(False)
-        ax.spines['left'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-        ax.xaxis.set_ticks_position('bottom')
+        # plot server
+        for date in data[indication][website]['dates']:
+            if 'server' in data[indication][website]['dates'][date]:
+                plt.axvline(x=datetime.datetime.strptime(str(date), '%Y%m%d'), color='#fee391')
 
-        ax.get_yaxis().set_ticklabels([])
-        day = pd.to_timedelta("1", unit='D')
-        plt.xlim(X[0] - day, X[-1] + day)
+        # plot asn
+        for date in data[indication][website]['dates']:
+            if 'asn' in data[indication][website]['dates'][date]:
+                plt.axvline(x=datetime.datetime.strptime(str(date), '%Y%m%d'), color='#ffffe5')
+
+        # plot google_psi_mobile
+        plot_list = []
+        dates_datetime = []
+        for date in data[indication][website]['dates']:
+            if 'google_psi_mobile' in data[indication][website]['dates'][date]:
+                plot_list.append(int(data[indication][website]['dates'][date]['google_psi_mobile']))
+                dates_datetime.append(datetime.datetime.strptime(str(date), '%Y%m%d'))
+        dates_float = matplotlib.dates.date2num(dates_datetime)
+        plt.plot_date(dates_float, plot_list, linestyle='-', xdate=True, ydate=False, color='#014636')
+
+        # plot google_psi_mobile_usability
+        plot_list = []
+        dates_datetime = []
+        for date in data[indication][website]['dates']:
+            if 'google_psi_mobile_usability' in data[indication][website]['dates'][date]:
+                plot_list.append(int(data[indication][website]['dates'][date]['google_psi_mobile_usability']))
+                dates_datetime.append(datetime.datetime.strptime(str(date), '%Y%m%d'))
+        dates_float = matplotlib.dates.date2num(dates_datetime)
+        plt.plot_date(dates_float, plot_list, linestyle='-', xdate=True, ydate=False, color='#016c59')
+
+        # plot google_psi_desktop
+        plot_list = []
+        dates_datetime = []
+        for date in data[indication][website]['dates']:
+            if 'google_psi_desktop' in data[indication][website]['dates'][date]:
+                plot_list.append(int(data[indication][website]['dates'][date]['google_psi_desktop']))
+                dates_datetime.append(datetime.datetime.strptime(str(date), '%Y%m%d'))
+        dates_float = matplotlib.dates.date2num(dates_datetime)
+        plt.plot_date(dates_float, plot_list, linestyle='-', xdate=True, ydate=False, color='#02818a')
+
+        # plot moz_links
+        plot_list = []
+        dates_datetime = []
+        for date in data[indication][website]['dates']:
+            if 'moz_links' in data[indication][website]['dates'][date]:
+                plot_list.append(int(data[indication][website]['dates'][date]['moz_links']))
+                dates_datetime.append(datetime.datetime.strptime(str(date), '%Y%m%d'))
+        dates_float = matplotlib.dates.date2num(dates_datetime)
+        plt.plot_date(dates_float, plot_list, linestyle='-', xdate=True, ydate=False, color='#C0C0C0')
+
+        # plot moz_rank
+        plot_list = []
+        dates_datetime = []
+        for date in data[indication][website]['dates']:
+            if 'moz_rank' in data[indication][website]['dates'][date]:
+                plot_list.append(float(data[indication][website]['dates'][date]['moz_rank']))
+                dates_datetime.append(datetime.datetime.strptime(str(date), '%Y%m%d'))
+        dates_float = matplotlib.dates.date2num(dates_datetime)
+        plt.plot_date(dates_float, plot_list, linestyle='-', xdate=True, ydate=False, color='#FFDF00')
+
+        # save the figure
         plt.savefig('data/' + website.replace("/","-") + '.png', bbox_inches='tight')
-
         plt.close('all')
+
+
+        # get the most recent server
+        code = ''
+        for date in reversed(data[indication][website]['dates']):
+            if 'code' in data[indication][website]['dates'][date]:
+                code = data[indication][website]['dates'][date]['code']
+                break
 
         # get the most recent https
         https = ''
@@ -632,12 +684,15 @@ for indication in data:
         content += '\n<tr>'
         content += '<td><a href="http://{0}" target="_blank">{0}</a><br/><sub>{1}</sub><br/><sub>{2}</sub></td>'.format( website , data[indication][website]['drug']['generic'] , data[indication][website]['drug']['company'] )
         content += '<td><a href="https://www.ssllabs.com/ssltest/analyze.html?d={0}" target="_blank">{1}</a><br/><sub>{2}</sub><br/><sub>{3}</sub></td>'.format( website , https, server, asn )
+        content += '<td><sub>{0}</sub></td>'.format( code )
         content += '<td><a href="https://developers.google.com/speed/pagespeed/insights/?url={0}&tab=mobile" target="_blank"><sub>{1}</sub></a></td>'.format( website , google_psi_mobile )
         content += '<td><a href="https://developers.google.com/speed/pagespeed/insights/?url={0}&tab=mobile" target="_blank"><sub>{1}</sub></a></td>'.format( website , google_psi_mobile_usability )
         content += '<td><a href="https://developers.google.com/speed/pagespeed/insights/?url={0}&tab=desktop" target="_blank"><sub>{1}</sub></a></td>'.format( website , google_psi_desktop )
         content += '<td><sub>{0}</sub></td>'.format( moz_links )
         content += '<td><sub>{0}</sub></td>'.format( moz_rank )
-        content += '<td><img src="data/{0}.png"/></td>'.format( website.replace("/","-") )
+        content += '</tr>'
+        content += '\n<tr>'
+        content += '<td colspan="8"><img src="data/{0}.png"/></td>'.format( website.replace("/","-") )
         content += '</tr>'
 
 content += '\n</table>'
@@ -657,6 +712,7 @@ Moai /ˈmoʊ.aɪ/ provides competitive intelligence by tracking the unique regul
 
 Additionally, the following the metrics are captured:
 
+* **Regulatory code**: Gain insight into how often a website is updated
 * **HTTPS**: Sadly, many website infrastructures do not provide HTTPS which [provides no data security](https://www.chromium.org/Home/chromium-security/marking-http-as-non-secure) to its visitors
 * **server**: The HTTP server header provides insight into infrastructure changes
 * **ASN**: Autonomous System Number provides insight into data center moves
